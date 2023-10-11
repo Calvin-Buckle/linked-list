d2ca8d1179a54ffcdf50ba377cfa5e1dd0db51f0
Calvin Buckle <ccccbuckle@gmail.com>
2023-10-11 15:27:45 +0200

    get linked list working

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
+
+class Node{
+    constructor(value)
+    {
+    this.value = value;
+    this.nextNode = null;
+    }
+    }
+    
+
 class LinkedList
 {
-constructor(head = null){
+constructor(head = null)
+{
     this.head = head
-    this.tail = null
+    
+    
+    this.size = 0
 }
-append(value){
+
+append(value)
+{
     let node = new Node(value)
     if (this.head == null)
     {
     return this.head = node;
-    }
-    else
+    };
+   if (this.head.nextNode!= null){this.head.nextNode.nextNode = node}
+   else
     {
-    return this.tail = node
+    return this.head.nextNode = node
     }
+    
 }
 
-}
-
-class Node{
-constructor(value)
+prepend(value)
 {
-this.value = value;
-this.nextNode = null;
-}
+    
+    let node = new Node(value)
+    {
+        this.head = node;
+    }
+   
 }
 
 
-let thing = new LinkedList;
-thing.append(10)
+
+
+}
+
+
+let thing = new LinkedList;;
+thing.prepend(10)
+thing.append(20)
+thing.append(30)
+
+
 console.log(thing)
 
