204d97d508965693d8b97315bd6c4ff96d496fcc
Calvin Buckle <ccccbuckle@gmail.com>
2023-10-11 12:58:16 +0200

    create working list

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 class LinkedList
 {
+constructor(head = null){
+    this.head = head
+}
+append(value){
+    let node = new Node(value)
+    return this.head = node;
+}
 
 }
 
 class Node{
 constructor(value)
 {
-this.value = null;
+this.value = value;
 this.nextNode = null;
 }
 }
+
+
+let thing = new LinkedList;
+thing.append(10)
+console.log(thing)
+
